d33e11a00e6ce03ceb77265fc6491d8b87dca9f7
Elnura <elya.ayase@gmail.com>
2024-02-08 02:59:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-aniimation needed
+/**/
+/* select no and yes buttons + counting clicks */
+
+const noButton = getElementById('noButton');
+const yesButton = getElementById ('yesButton');
+let clickCount = 0;
+
