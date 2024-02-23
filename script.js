319d20c32b1d252455d559eb5bfe45adc1077b60
Elnura <elya.ayase@gmail.com>
2024-02-24 02:49:48 +0400

    Update script.js
    Signed-off-by: Elnura <elya.ayase@gmail.com>

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ noButton.addEventListener('click',()  =>{
   clickCount++;
   
   //changing the size of yes
-  yesButton.style.transform = 'scale(${1 + clickCount * 0.05})';
+  yesButton.style.transform = `scale(${1 + clickCount * 0.05})`;
 
   //changing position of no button
-  noButton.style.marginLeft = '${clickCount *5}px' //margin might be adjusted later
+  noButton.style.marginLeft = `${clickCount *5}px` //margin might be adjusted later
   
   //array phrases for NO
   const phrases = ["Are you sure?", "Why not?", "Think again", "pleasepleaseplease", "DON'T AGREE!!!", "You've got another chance", "How dare you?", "BUT WHYYYYY?", "Sorry, huh?", "Ops, you clicked the wrong button", "Think wisely", "Don't be stubborn", "I know you wanted to click YES ", "this is not a YES button", "DO NOT CLICK", "why did you click it?", "you have a death wish?", "what's wrong with your fingers?", "ok, what's wrong with your eyes", "Not accepted", "put on your glasses", "you could finish it earlier by pressing yes", "let me be honest with you , there is no button 'NO'", "good luck with that", "seriously?", "it never be clickable", "you literally can't say no", "you wish", "who cares?", "you think you can decide?", "nice try", "you're wasting your time", "there is only yes in my plans", "yes"];
